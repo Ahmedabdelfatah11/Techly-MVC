bc4af1c50a11dba58988e9fb3facbce85c03c735
Ahmed mohamed <ahmed22mohamed9584@gmail.com>
2025-09-07 19:29:07 +0300

    chore: initial commit

---
+++ Techly.DAL/Context/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Techly.DAL.Models;
+
+namespace Techly.DAL.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Category>  Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                 new Category { Id = 1, Name = "Mobile Accessories", DisplayOrder = 1 },
+                 new Category { Id = 2, Name = "Power & Charging", DisplayOrder = 2 },
+                 new Category { Id = 3, Name = "Headphones & Audio", DisplayOrder = 3 },
+                 new Category { Id = 4, Name = "Smart Gadgets", DisplayOrder = 4 }
+            );
+        }
+    }
+}
---
+++ TechlyApplication/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Techly.DAL.Context;
+using Techly.DAL.Models;
+
+namespace Techly.Presentation.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _dbContext.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
