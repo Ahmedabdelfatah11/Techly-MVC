89186a2af7b046376e41562761083d596e8408eb
Ahmed mohamed <ahmed22mohamed9584@gmail.com>
2025-09-09 00:56:02 +0300

    Add Edit and Delete functionality for Category
    - Added Edit and Delete buttons in Category list view
    - Implemented Get and Post actions for Edit
    - Updated Category details and save changes to database
    - Implemented Get and Post actions for Delete
    - Integrated Toastr notifications for create, update, and delete actions

--- TechlyApplication/Controllers/CategoryController.cs
+++ TechlyApplication/Controllers/CategoryController.cs
@@ -17,20 +17,72 @@ namespace Techly.Presentation.Controllers
             List<Category> categories = _dbContext.Categories.ToList();
             return View(categories);
         }
-        [HttpPost]
-        public IActionResult Create(Category category)
-        {
-            if(ModelState.IsValid)
-            {
-            _dbContext.Categories.Add(category);
-            _dbContext.SaveChanges();
-            return RedirectToAction("Index");
-            }
-            return View(category);
-        }
         public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            //if(category.Name== category.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The Display order cannot exactly match the Name.");
+            //}
+            if (ModelState.IsValid)
+            {
+            _dbContext.Categories.Add(category);
+            _dbContext.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+        public IActionResult Edit(int? id)
+        {
+            Category? categoryFromDb= _dbContext.Categories.Find(id);
+            if(id == null || id==0 || categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category, int id)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                TempData["success"] = "Category edited successfully";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+       
+        
+        public IActionResult Delete(int? id)
+        {
+            Category? categoryFromDb= _dbContext.Categories.Find(id);
+            if(id == null || id==0 || categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST( int id)
+        {
+            Category category=_dbContext.Categories.Find(id);
+            if(category == null)
+            { 
+                return NotFound();
+            }
+            _dbContext.Categories.Remove(category);
+                _dbContext.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
