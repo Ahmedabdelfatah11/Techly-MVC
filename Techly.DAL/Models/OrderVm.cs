f55ac8791d8e577edd448e983f73d14a4dd6d555
Ahmed mohamed <ahmed22mohamed9584@gmail.com>
2025-09-17 07:26:24 +0300

    feat(order): implement order management features
    - Added OrderVM and Order Controller
    - Implemented Order List UI
    - Added Status Filter
    - Made Status Selected Active
    - Demo: Filters in Order List

---
+++ Techly.DAL/Models/OrderVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Techly.DAL.Models
+{
+    public class OrderVm
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
+    }
+}
---
+++ TechlyApplication/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Techly.BLL.Interfaces;
+using Techly.DAL.Models;
+using Utility;
+
+namespace Techly.Presentation.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll(string status)
+        {
+           IEnumerable<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+
+
+            return Json(new { data = orderHeaders });
+        }
+
+        #endregion
+    }
+}
